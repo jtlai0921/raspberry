de047fe78797fdf598b1960e4b3364d1023a9dfb
Robert Hsu <roberthsu2003@gmail.com>
2019-11-23 12:16:13 +0800

    modify

--- pythonProject/content.py
+++ pythonProject/content.py
@@ -83,3 +83,12 @@ while True:
     if stuff == 'q':
         break;
     print(stuff.capitalize())
+
+ while True:
+...     value = input("Integer, please [q to quit]:")
+...     if value == 'q':
+...             break
+...     number = int(value)
+...     if number % 2 == 0:
+...             continue
+...     print(number, "squared is", number*number)
