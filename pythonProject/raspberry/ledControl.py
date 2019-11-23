9ffc35057dcc85d076c49c6f402e46930e864410
Robert Hsu <roberthsu2003@gmail.com>
2019-11-23 15:33:26 +0800

    modify

--- pythonProject/raspberry/ledControl.py
+++ pythonProject/raspberry/ledControl.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3.7
+from gpiozero import LED
+from time import sleep
 
 if __name__ == "__main__":
-    pass
+    led = LED(25)
+    while True:
+        led.on()
+        sleep(1)
+        led.off()
+        sleep(1)
