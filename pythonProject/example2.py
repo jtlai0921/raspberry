c23af92235c16cbca9b32859e380b60707bfdd4d
Robert Hsu <roberthsu2003@gmail.com>
2019-11-23 10:15:32 +0800

    modify

---
+++ pythonProject/example2.py
@@ -0,0 +1,12 @@
+empty_list = []
+weekday= ['monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+big_birds = ['emu', 'ostrich', 'cassowary']
+first_names = ['Graham', 'John', 'Terry', 'Terry', 'Michael']
+
+another_empty_list = list()
+print(another_empty_list)
+print(empty_list)
+print(weekday)
+print(big_birds)
+print(first_names)
+
---
+++ pythonProject/example4.py
@@ -0,0 +1,6 @@
+#!usr/bin/python3
+disaster = True
+if disaster:
+    print("Woe!")
+else:
+    print("Wheel!")
