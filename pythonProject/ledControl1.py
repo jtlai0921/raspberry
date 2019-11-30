482faa14d46b1a05f1905baef1eec2b51e8e55ac
Robert Hsu <roberthsu2003@gmail.com>
2019-11-30 15:46:45 +0800

    modify

--- pythonProject/ledControl1.py
+++ pythonProject/ledControl1.py
@@ -2,10 +2,16 @@
 
 from tkinter import *
 from tkinter import Button
+from gpiozero import LED
+
+led = LED(25);
 
 if __name__ == "__main__":
     master = Tk()
     master.title("LED 開關")
     Button(master, text="you shot me", padx=70, pady=30).pack(padx=20, pady=20)
+
+    #led
+    led.on()
     mainloop()
 
