681a7d54d98afb707eaf149e72e4e46117f4d4ba
Robert Hsu <roberthsu2003@gmail.com>
2019-11-30 14:07:55 +0800

    modify

--- pythonProject/window1.py
+++ pythonProject/window1.py
@@ -2,6 +2,13 @@
 '''
 這是製作文件的地方
 '''
+from tkinter import *
 
 if __name__ == "__main__":
-    print('建立window');
+    root = Tk()
+    root.title("LED Control")
+    for relief in [RAISED, SUNKEN, FLAT, RIDGE, GROOVE, SOLID]:
+        f = Frame(root, borderwidth = 2, relief = relief)
+        Label(f, text=relief, width=10).pack(side=LEFT)
+        f.pack(side=LEFT, padx=5, pady=5)
+    root.mainloop()
