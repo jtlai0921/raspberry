65269a003909a0f0d2dfae7460c8a35e972b3a52
Robert Hsu <roberthsu2003@gmail.com>
2019-11-30 15:14:47 +0800

    modify

--- pythonProject/ledControl1.py
+++ pythonProject/ledControl1.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3.7
 
 from tkinter import *
+from tkinter import Button
 
 if __name__ == "__main__":
     master = Tk()
-    ledBtn = Button(master)
-    ledBtn.configure(text="You shot me!")
-    ledBtn.pack()
+    master.title("LED 開關")
+    Button(master, text="you shot me", padx=70, pady=30).pack(padx=20, pady=20)
     mainloop()
 
