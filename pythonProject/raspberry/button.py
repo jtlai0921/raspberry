67dd55a8cc8245328b4f974de23cedae245e54b1
Robert Hsu <roberthsu2003@gmail.com>
2019-11-30 11:04:11 +0800

    modify

--- pythonProject/raspberry/button.py
+++ pythonProject/raspberry/button.py
@@ -3,9 +3,9 @@
 from gpiozero import Button
 
 if __name__ == "__main__":
-    btn = Button(18)
+    btn = Button(17)
     while True:
-        if btn.is_pressed:
+        if btn.is_active:
             print("按鈕被按");
         else:
             print("按鈕沒有被按")
