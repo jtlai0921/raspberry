6dca25ff8b4ed344aaa2f17e34aa206081ca1d6f
Robert Hsu <roberthsu2003@gmail.com>
2019-12-07 15:59:06 +0800

    modify

--- pythonProject/pack.py
+++ pythonProject/pack.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3.7
 
+from tkinter import *
+
+def createInterface(w):
+    mainFrame = Frame(w,width=300, height=110)
+    mainFrame.pack()
+
 if __name__ == '__main__':
-    print("Hello!World!");
+   window = Tk()
+   createInterface(w=window)
+   window.mainloop()
