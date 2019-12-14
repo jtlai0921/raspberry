150ec45806322637d0d3511298f16c33e8aa7fd9
Robert Hsu <roberthsu2003@gmail.com>
2019-12-14 15:00:47 +0800

    modify

--- pythonProject/tkinterLayout5.py
+++ pythonProject/tkinterLayout5.py
@@ -1,10 +1,12 @@
 from tkinter import *
 
+
 class App:
     def __init__(self, master):
-        Button(master, text='Left').pack(side=TOP, expand=YES, fill=BOTH)
-        Button(master, text='Center').pack(side=TOP, expand=YES, fill=BOTH)
-        Button(master, text='Right').pack(side=TOP, expand=YES, fill=BOTH)
+        Button(master, text='Left').pack(side=TOP, anchor=W)
+        Button(master, text='Center').pack(side=TOP, anchor=CENTER)
+        Button(master, text='Right').pack(side=TOP, anchor=E)
+
 
 if __name__ == '__main__':
     window = Tk()
