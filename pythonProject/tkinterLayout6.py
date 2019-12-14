56be96964124df13bdc4412e1d94e907fbf23757
Robert Hsu <roberthsu2003@gmail.com>
2019-12-14 16:32:25 +0800

    modify

--- pythonProject/tkinterLayout6.py
+++ pythonProject/tkinterLayout6.py
@@ -11,9 +11,9 @@ class App:
         Entry(frame2).grid(row=1, column=1)
         Checkbutton(frame2, text='Preserve aspec').grid(row=2, column=0, columnspan=2, sticky=W)
         photo = PhotoImage(file='images.gif')
-        label = Label(image=photo)
+        label = Label(frame2, image=photo)
         label.image = photo
-        label.grid(row=0, column=2, rowspan=2)
+        label.grid(row=0, column=2)
         frame2.pack(side=BOTTOM, expand=YES, fill=BOTH)
 
 
@@ -21,6 +21,5 @@ if __name__ == '__main__':
     window = Tk()
     window.option_add('*font', ('verdana', 12, 'bold'))
     window.title('Pack - Example 1')
-    window.geometry("300x300")
     display = App(window)
     window.mainloop()
