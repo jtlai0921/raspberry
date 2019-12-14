938ca06a5dcbad3492965a7f6c5ccda0abe92ca9
Robert Hsu <roberthsu2003@gmail.com>
2019-12-14 15:42:22 +0800

    modify

--- pythonProject/tkinterLayout6.py
+++ pythonProject/tkinterLayout6.py
@@ -4,10 +4,16 @@ from tkinter import *
 class App:
     def __init__(self, master):
         frame = Frame(master)
-        Button(frame, text='Left').pack(side=TOP, anchor=W, expand=YES, fill=X)
-        Button(frame, text='Center').pack(side=TOP, anchor=W, expand=YES, fill=X)
-        Button(frame, text='Right').pack(side=TOP, anchor=W, expand=YES, fill=X)
-        frame.pack(expand=YES, fill=X)
+        Button(frame, text='Left').pack(side=TOP, anchor=W, expand=YES, fill=BOTH)
+        Button(frame, text='Center').pack(side=TOP, anchor=W, expand=YES, fill=BOTH)
+        Button(frame, text='Right').pack(side=TOP, anchor=W, expand=YES, fill=BOTH)
+        frame.pack(side=LEFT, expand=YES, fill=BOTH)
+
+        frame1 = Frame(master)
+        Button(frame1, text='Left').pack(side=LEFT, anchor=W, expand=YES, fill=BOTH)
+        Button(frame1, text='Center').pack(side=LEFT, anchor=W, expand=YES, fill=BOTH)
+        Button(frame1, text='Right').pack(side=LEFT, anchor=W, expand=YES, fill=BOTH)
+        frame1.pack(side=RIGHT, expand=YES, fill=BOTH)
 
 
 if __name__ == '__main__':
