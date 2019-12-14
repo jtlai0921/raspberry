dcfafa1e92c50f2f559737dc721eec85fe31adc0
Robert Hsu <roberthsu2003@gmail.com>
2019-12-14 14:46:10 +0800

    modify

--- pythonProject/tkinterLayout4.py
+++ pythonProject/tkinterLayout4.py
@@ -2,13 +2,14 @@ from tkinter import *
 
 class App:
     def __init__(self, master):
-        Button(master, text='Left').pack(side=LEFT)
-        Button(master, text='Center').pack(side=LEFT)
-        Button(master, text='Right').pack(side=RIGHT)
+        Button(master, text='Left').pack(side=TOP, expand=YES)
+        Button(master, text='Center').pack(side=TOP, expand=YES)
+        Button(master, text='Right').pack(side=TOP, expand=YES)
 
 if __name__ == '__main__':
     window = Tk()
     window.option_add('*font', ('verdana', 12, 'bold'))
     window.title('Pack - Example 1')
+    window.geometry("300x200")
     display = App(window)
     window.mainloop()
