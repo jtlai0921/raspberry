9fd909f1b3cdfbd17ad198b4adab79ad977e8437
Robert Hsu <roberthsu2003@gmail.com>
2019-12-14 16:00:16 +0800

    modify

--- pythonProject/tkinterLayout6.py
+++ pythonProject/tkinterLayout6.py
@@ -15,11 +15,16 @@ class App:
         Button(frame1, text='Right').pack(side=LEFT, anchor=W, expand=YES, fill=BOTH)
         frame1.pack(side=RIGHT, expand=YES, fill=BOTH)
 
+        frame2 = Frame(master)
+        Label(frame2, text="First").grid(row=0, column=0, sticky=E)
+        Label(frame2, text="Second").grid(row=1, column=0, sticky=E)
+        frame2.pack(side=TOP, expand=YES, fill=BOTH)
+
 
 if __name__ == '__main__':
     window = Tk()
     window.option_add('*font', ('verdana', 12, 'bold'))
     window.title('Pack - Example 1')
-    window.geometry("300x200")
+    window.geometry("300x300")
     display = App(window)
     window.mainloop()
