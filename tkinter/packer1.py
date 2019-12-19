58df2051f92b4dedcd8148c3125b02820dac837f
robert <roberthsu2003@gmail.com>
2019-12-19 11:53:48 +0800

    modify

--- tkinter/packer1.py
+++ tkinter/packer1.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     window.title("Pack-Example1")
     mainFrame = Frame(window, borderwidth=2, relief=GROOVE)
     Button(mainFrame, text='Left', padx=10, pady=10).pack(side=LEFT, padx=10, pady=10)
-    Button(mainFrame, text='Center', padx=10, pady=10).pack(side=LEFT, padx=10, pady=10)
+    Button(mainFrame, text='This is the Center button', padx=10, pady=10).pack(side=LEFT, padx=10, pady=10)
     Button(mainFrame, text='Right', padx=10, pady=10).pack(side=LEFT, padx=10, pady=10)
     mainFrame.pack(padx=20, pady=20)
     window.mainloop()
