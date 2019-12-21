3bbc84d27c82ba2a0250cbe7d06a354d1ca31d1e
Robert Hsu <roberthsu2003@gmail.com>
2019-12-21 11:31:38 +0800

    modify

--- pythonProject/relayControl.py
+++ pythonProject/relayControl.py
@@ -5,11 +5,16 @@ class App:
     def __init__(self, window):
         self.relay = Relay(25)
 
+        #interface
+        mainFrame = Frame(window, borderwidth=2, relief=GROOVE)
+        Button(mainFrame, text="打開").pack(expand=YES, fill=BOTH, padx=40, pady=25)
+        mainFrame.pack(expand=YES, fill=BOTH, padx=5, pady=20)
+
 if __name__ == "__main__":
     window = Tk()
     window.title("relayControl")
     window.geometry("300x200")
-    window.option_add("*Font", ('verdana', 18, 'bold'))
+    window.option_add("*Font", ('verdana', 20))
     window.option_add('*Label.Font', ('verdana', 18))
     window.option_add('*Button.Background', "dark gray")
     app = App(window)
