f7549ba1ec545e550dbf9d0f3684979e45fcaed1
Robert Hsu <roberthsu2003@gmail.com>
2019-12-21 11:46:14 +0800

    modify

--- pythonProject/relayControl.py
+++ pythonProject/relayControl.py
@@ -1,14 +1,25 @@
 from tkinter import *
 from gpiozero import LED as Relay
 
+
 class App:
     def __init__(self, window):
         self.relay = Relay(25)
 
         #interface
+        self.buttonText = StringVar();
+
         mainFrame = Frame(window, borderwidth=2, relief=GROOVE)
-        Button(mainFrame, text="打開").pack(expand=YES, fill=BOTH, padx=40, pady=25)
+        Button(mainFrame, textvariable=self.buttonText, command=self.userClick).pack(expand=YES, fill=BOTH, padx=40, pady=25)
+        self.buttonText.set("OPEN")
         mainFrame.pack(expand=YES, fill=BOTH, padx=5, pady=20)
+
+    def userClick(self):
+        if self.buttonText.get() == "CLOSE":
+            self.buttonText.set("OPEN")
+        else:
+            self.buttonText.set("CLOSE")
+
 
 if __name__ == "__main__":
     window = Tk()
