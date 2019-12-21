abb436fa1b769f32ad5ce598e3f7ef0aa5991d78
Robert Hsu <roberthsu2003@gmail.com>
2019-12-21 10:53:10 +0800

    modify

--- pythonProject/relayControl.py
+++ pythonProject/relayControl.py
@@ -1,6 +1,16 @@
 from tkinter import *
+from gpiozero import LED as Relay
+
+class App:
+    def __init__(self, window):
+        self.relay = Relay(25)
 
 if __name__ == "__main__":
     window = Tk()
     window.title("relayControl")
+    window.geometry("300x200")
+    window.option_add("*Font", ('verdana', 18, 'bold'))
+    window.option_add('*Label.Font', ('verdana', 18))
+    window.option_add('*Button.Background', "dark gray")
+    app = App(window)
     window.mainloop()
