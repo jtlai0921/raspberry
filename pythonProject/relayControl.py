2389a1ac0c12ab2beef146704aa0f15583735493
Robert Hsu <roberthsu2003@gmail.com>
2019-12-21 15:20:05 +0800

    modify

--- pythonProject/relayControl.py
+++ pythonProject/relayControl.py
@@ -14,22 +14,30 @@ class App:
         default_app = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://raspberryfirebase.firebaseio.com/'
 })
-        self.ledControlRef = db.reference('iot1221/relayControl')
-        print(self.ledControlRef.get());
-
         #interface
-        self.buttonText = StringVar();
+        self.buttonText = StringVar()
 
         mainFrame = Frame(window, borderwidth=2, relief=GROOVE)
         Button(mainFrame, textvariable=self.buttonText, command=self.userClick).pack(expand=YES, fill=BOTH, padx=40, pady=25)
         self.buttonText.set("OPEN")
         mainFrame.pack(expand=YES, fill=BOTH, padx=5, pady=20)
 
+        self.relayControlRef = db.reference('iot1221/relayControl')
+        self.relayControlRef.listen(self.firebaseDataChanges)
+
     def userClick(self):
-        if self.buttonText.get() == "CLOSE":
-            self.buttonText.set("OPEN")
+        if self.relayControlRef.get():
+            self.relayControlRef.set(False)
         else:
+            self.relayControlRef.set(True)
+
+    def firebaseDataChanges(self, event):
+        print("Event Data:{},Event paht:{}".format(event.data, event.path))
+        if event.data:
             self.buttonText.set("CLOSE")
+        else:
+            self.buttonText.set("OPEN")
+        pass
 
 
 if __name__ == "__main__":
