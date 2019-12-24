c570add5253d7c74491c51518b4ecbce5671fcb3
robert <roberthsu2003@gmail.com>
2019-12-24 11:29:11 +0800

    modify

--- tkinter/packer2.py
+++ tkinter/packer2.py
@@ -1,15 +1,19 @@
 #!evn01/Scripts/python3.8
+""" docString"""
 from tkinter import *
 
 
 def interface(w):
+    """ 建立介面 """
     w.option_add("*font",('verdana',16))
     w.title("Pack - Example")
     w.geometry("600x200")
+    w.wm_resizable(height=False, width=False)
+
 
 
 if __name__ == "__main__":
     window = Tk()
-    interface(window)
+    interface(w=window)
     window.mainloop()
 
