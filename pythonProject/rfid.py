b27aa288c714cf262e32e94e073989a3a70023e9
Robert Hsu <roberthsu2003@gmail.com>
2019-12-28 12:30:52 +0800

    modify

--- pythonProject/rfid.py
+++ pythonProject/rfid.py
@@ -1,4 +1,8 @@
 from LCD.lcd_display import lcd
+from gpiozero import Buzzer
+
 my_lcd = lcd()
 my_lcd.display_string("Raspberry Pi", 1)
 my_lcd.display_string("Robert HSU", 2)
+bz = Buzzer(16)
+bz.beep()
