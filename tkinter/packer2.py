92bd8ccb0b5da42087595c8e263334afb508b918
robert <roberthsu2003@gmail.com>
2019-12-31 11:27:29 +0800

    modify

--- tkinter/packer2.py
+++ tkinter/packer2.py
@@ -5,8 +5,10 @@ from tkinter import *
 def readText():
     with open('news.txt','rt',encoding="utf-8") as fileObject:
         content = fileObject.read()
-
     print(content)
+
+def writeContent():
+    print('您輸入的是:%s' % entryVariable.get())
 
 def interface(w):
     """ 建立介面 """
@@ -18,22 +20,22 @@ def interface(w):
     menuFrame = Frame(w,bg='purple',width=200)
     lineFrame = Frame(menuFrame,borderwidth=2,relief=GROOVE,width=200,bg='purple')
     Button(lineFrame,text='讀入text', command=readText).pack(fill=BOTH,expand=TRUE)
-    Button(lineFrame, text='ok2').pack(fill=BOTH,expand=TRUE,pady=5)
+    Button(lineFrame, text='寫入text', command=writeContent).pack(fill=BOTH,expand=TRUE,pady=5)
     Button(lineFrame, text='ok3').pack(fill=BOTH,expand=TRUE)
     lineFrame.pack(expand=YES, fill=BOTH,padx=10,pady=10)
     menuFrame.pack(side=LEFT, expand=YES,fill=BOTH)
 
     rightFrame = Frame(w, bg='gray',width=400)
     Label(rightFrame,text="輸入文字:").pack(side=LEFT, padx=5, pady=10)
-    e = StringVar();
-    Entry(rightFrame,width=50,textvariable=e).pack(side=LEFT)
+    Entry(rightFrame,width=50,textvariable=entryVariable).pack(side=LEFT)
     rightFrame.pack(side=LEFT, expand=YES, fill=BOTH)
-    e.set("Hello!World")
+    entryVariable.set("Hello!World")
 
 
 
 if __name__ == "__main__":
     window = Tk()
+    entryVariable = StringVar()
     interface(w=window)
     window.mainloop()
 
