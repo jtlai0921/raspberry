863d0a93dd99eb26664ef8d5ffca73de3b32fcb9
robert <roberthsu2003@gmail.com>
2020-01-02 11:22:44 +0800

    modify

--- tkinter/weather.py
+++ tkinter/weather.py
@@ -1,5 +1,5 @@
-from report import get_description
+from report import get_description as get_de
 
-description = get_description()
+description = get_de()
 print("Today's weather:", description)
 print("weather.py的__name__=",__name__)
